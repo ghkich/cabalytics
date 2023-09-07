53bfae88af85c870d1b777ce0241b813b277fe92
gustavo <gh.kich@gmail.com>
2023-09-07 02:26:06 -0300

    feat: add collection-appraiser route and page

--- app/[lang]/collection-appraiser/page.tsx
+++ app/[lang]/collection-appraiser/page.tsx
@@ -5,7 +5,7 @@ export default async function Home({ params: { lang } }: { params: { lang: Local
     const { navigation } = await getDictionary(lang)
     return (
         <div className="flex max-w-lg flex-col p-4">
-            <h1 className="pb-2 uppercase text-neutral-300">{navigation.cabalytics}</h1>
+            <h1 className="pb-2 uppercase text-neutral-300">{navigation.collection_appraiser}</h1>
             <p className="text-neutral-450 text-sm">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Quisquam, voluptatum.
                 Quisquam,
