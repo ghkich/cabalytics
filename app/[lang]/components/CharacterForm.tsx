b9f56bb3ada79ed9d0e6e58db8db64b123325c68
gustavo <gh.kich@gmail.com>
2023-09-08 23:15:27 -0300

    feat: test loading all battleStyle images before

--- app/[lang]/components/CharacterForm.tsx
+++ app/[lang]/components/CharacterForm.tsx
@@ -8,6 +8,8 @@ import useMergeState from '@/lib/useMergeState'
 import { useCombatPower } from '@/lib/useCombatPower'
 import Image from 'next/image'
 import { cls } from '@/lib/utils'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown } from '@fortawesome/sharp-light-svg-icons'
 
 const initialAttackAttributes: AttackAttributes = {
     attack: 0,
@@ -160,26 +162,74 @@ export const CharacterForm = ({ initialBattleStyle, onChange }: Props) => {
 
     return (
         <div className="flex flex-col gap-0.5">
+            <div className="flex w-full gap-0.5">
+                <div className="flex w-full items-center justify-center bg-neutral-900 py-1 text-center text-[10px] uppercase text-neutral-600">
+                    <h1>Atacante</h1>
+                </div>
+                <div className="flex gap-0.5 text-center text-xs text-neutral-500">
+                    <button
+                        type="button"
+                        className={cls('w-8 bg-neutral-900 py-0.5', {
+                            'bg-neutral-825 text-neutral-400': true,
+                        })}
+                    >
+                        1
+                    </button>
+                    <button type="button" className="w-8 bg-neutral-900">
+                        2
+                    </button>
+                    <button type="button" className="w-8 bg-neutral-900">
+                        3
+                    </button>
+                    <button type="button" className="w-8 bg-neutral-900">
+                        4
+                    </button>
+                </div>
+            </div>
             <div className="flex flex-col">
                 <div className="text-neutral-450 flex gap-0.5 text-[10px] font-light">
                     <button
                         type="button"
                         onClick={() => setShowBattleStyleSelector((prev) => !prev)}
-                        className="bg-neutral-875 hover:bg-neutral-825 flex h-12 w-12 shrink-0 items-center justify-center transition-colors duration-200 active:bg-neutral-900"
+                        className="bg-neutral-875 hover:bg-neutral-825 relative flex h-12 w-12 shrink-0 items-center justify-center transition-colors duration-200 active:bg-neutral-900"
                     >
-                        {battleStyle && (
-                            <Image
-                                key={battleStyle}
-                                src={battleStyles[battleStyle].icon}
-                                alt={battleStyles[battleStyle].description[lang]}
-                                className="animate-spin-selection"
-                                width={32}
+                        {Object.entries(battleStyles).map(([key, style]) => {
+                            return (
+                                <Image
+                                    key={`${key}-${battleStyle}`}
+                                    src={style.icon}
+                                    alt={style.description[lang]}
+                                    loading="eager"
+                                    className={cls('animate-spin-selection absolute opacity-0', {
+                                        'opacity-100': battleStyle === key,
+                                    })}
+                                    width={32}
+                                />
+                            )
+                        })}
+                        {!battleStyle && (
+                            <FontAwesomeIcon
+                                icon={faArrowDown}
+                                className={cls('mb-[-10px] animate-bounce text-xl text-neutral-600', {
+                                    'text-emerald-300': battleStyle,
+                                })}
                             />
                         )}
                     </button>
                     <div className="bg-neutral-910 flex w-full flex-col justify-center px-2 py-1">
-                        <div className="">Combat Power</div>
-                        <div className="text-[11px] font-light text-orange-200">{combatPower.total}</div>
+                        {battleStyle ? (
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <div className="">Combat Power</div>
+                                    <div className="text-[11px] font-light text-orange-200">{combatPower.total}</div>
+                                </div>
+                                <div className="pr-3"></div>
+                            </div>
+                        ) : (
+                            <div className="font-light text-neutral-500">
+                                Selecione o <b className="text-neutral-450 font-normal">estilo de luta</b> do atacante
+                            </div>
+                        )}
                     </div>
                 </div>
                 <div
@@ -194,7 +244,7 @@ export const CharacterForm = ({ initialBattleStyle, onChange }: Props) => {
                         <div
                             key={style.acronym[lang]}
                             className={cls(
-                                'hover:bg-neutral-850 mt-0.5 flex cursor-pointer items-center justify-center bg-neutral-900 px-0.5 py-1 opacity-75 transition-all duration-200 hover:opacity-100',
+                                'hover:bg-neutral-850 mt-0.5 flex cursor-pointer items-center justify-center bg-neutral-900 px-0.5 py-1 transition-all duration-200 hover:opacity-100',
                                 {
                                     'bg-neutral-825 opacity-100': battleStyle === key,
                                 }
@@ -204,89 +254,91 @@ export const CharacterForm = ({ initialBattleStyle, onChange }: Props) => {
                             <Image
                                 src={style.icon}
                                 alt={style.description[lang]}
-                                className="transition-all duration-200"
+                                className="opacity-90 transition-all duration-200"
                             />
                         </div>
                     ))}
                 </div>
             </div>
-            <div className="flex justify-evenly gap-0.5">
-                {attributeTypes.map((type) => (
-                    <TabButton
-                        key={type.value}
-                        active={attributeType === type.value}
-                        onClick={() => setAttributeType(type.value)}
-                    >
-                        <div className="text-[12px]">{type.label}</div>
-                        <div className="text-[9px] font-light text-neutral-500 text-opacity-75">
-                            {combatPower[type.value].total}
-                        </div>
-                    </TabButton>
-                ))}
-            </div>
-            <div className="flex justify-evenly gap-0.5">
-                {attributeCategories.map((category) => (
-                    <TabButton
-                        key={category.value}
-                        active={attributeCategory === category.value}
-                        onClick={() => setAttributeCategory(category.value)}
-                    >
-                        <div className="text-[11px]">{category.label[lang]}</div>
-                        <div className="text-[9px] font-light text-neutral-500 text-opacity-75">
-                            {combatPower[attributeType][category.value]}
-                        </div>
-                    </TabButton>
-                ))}
-            </div>
-            <div className="">
-                {attributeType === 'attack' && (
-                    <form className="flex flex-col gap-0.5">
-                        {Object.entries(attackAttributes).map(([key, { description }]) => {
-                            const typedKey = key as keyof AttackAttributes
-                            const { min, max } = attackAttributes[typedKey]
-                            const value = characterStats.attack[attributeCategory][typedKey]
+            <div className="flex flex-col gap-0.5">
+                <div className="flex justify-evenly gap-0.5">
+                    {attributeTypes.map((type) => (
+                        <TabButton
+                            key={type.value}
+                            active={attributeType === type.value}
+                            onClick={() => setAttributeType(type.value)}
+                        >
+                            <div className="text-[12px]">{type.label}</div>
+                            <div className="text-[9px] font-light text-neutral-500 text-opacity-75">
+                                {combatPower[type.value].total}
+                            </div>
+                        </TabButton>
+                    ))}
+                </div>
+                <div className="flex justify-evenly gap-0.5">
+                    {attributeCategories.map((category) => (
+                        <TabButton
+                            key={category.value}
+                            active={attributeCategory === category.value}
+                            onClick={() => setAttributeCategory(category.value)}
+                        >
+                            <div className="text-[11px]">{category.label[lang]}</div>
+                            <div className="text-[9px] font-light text-neutral-500 text-opacity-75">
+                                {combatPower[attributeType][category.value]}
+                            </div>
+                        </TabButton>
+                    ))}
+                </div>
+                <div className="">
+                    {attributeType === 'attack' && (
+                        <form className="flex flex-col gap-0.5">
+                            {Object.entries(attackAttributes).map(([key, { description }]) => {
+                                const typedKey = key as keyof AttackAttributes
+                                const { min, max } = attackAttributes[typedKey]
+                                const value = characterStats.attack[attributeCategory][typedKey]
 
-                            if (isMagicBased && typedKey === 'attack') return null
-                            if (!isMagicBased && typedKey === 'magicAttack') return null
-                            if (isMagicBased && typedKey === 'swordSkillAmp') return null
-                            if (!isMagicBased && typedKey === 'magicSkillAmp') return null
-                            if (isMagicBased && typedKey === 'minimumDamage') return null
+                                if (isMagicBased && typedKey === 'attack') return null
+                                if (!isMagicBased && typedKey === 'magicAttack') return null
+                                if (isMagicBased && typedKey === 'swordSkillAmp') return null
+                                if (!isMagicBased && typedKey === 'magicSkillAmp') return null
+                                if (isMagicBased && typedKey === 'minimumDamage') return null
 
-                            return (
-                                <AttributeInput
-                                    key={key}
-                                    name={key}
-                                    label={description[lang]}
-                                    onChange={handleChange}
-                                    value={value}
-                                    min={min}
-                                    max={max}
-                                />
-                            )
-                        })}
-                    </form>
-                )}
-                {attributeType === 'defense' && (
-                    <form className="flex flex-col gap-0.5">
-                        {Object.entries(defenseAttributes).map(([key, { description }]) => {
-                            const typedKey = key as keyof DefenseAttributes
-                            const { min, max } = defenseAttributes[typedKey]
-                            const value = characterStats.defense[attributeCategory][typedKey]
+                                return (
+                                    <AttributeInput
+                                        key={key}
+                                        name={key}
+                                        label={description[lang]}
+                                        onChange={handleChange}
+                                        value={value}
+                                        min={min}
+                                        max={max}
+                                    />
+                                )
+                            })}
+                        </form>
+                    )}
+                    {attributeType === 'defense' && (
+                        <form className="flex flex-col gap-0.5">
+                            {Object.entries(defenseAttributes).map(([key, { description }]) => {
+                                const typedKey = key as keyof DefenseAttributes
+                                const { min, max } = defenseAttributes[typedKey]
+                                const value = characterStats.defense[attributeCategory][typedKey]
 
-                            return (
-                                <AttributeInput
-                                    key={key}
-                                    name={key}
-                                    label={description[lang]}
-                                    onChange={handleChange}
-                                    value={value}
-                                    min={min}
-                                    max={max}
-                                />
-                            )
-                        })}
-                    </form>
-                )}
+                                return (
+                                    <AttributeInput
+                                        key={key}
+                                        name={key}
+                                        label={description[lang]}
+                                        onChange={handleChange}
+                                        value={value}
+                                        min={min}
+                                        max={max}
+                                    />
+                                )
+                            })}
+                        </form>
+                    )}
+                </div>
             </div>
         </div>
     )
